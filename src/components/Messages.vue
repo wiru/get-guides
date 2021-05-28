0b8967bbbe6f771d3d2ba0736ab4ffed0ec3af6e
Chris-Ack <chris_ackermann@web.de>
2021-05-28 12:02:35 +0900

    DO NOT PUSH THIS TO MAIN.

---
+++ src/components/Messages.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="messages">
+        <h1> I'm some dank messages </h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Messages',
+    // This is regular vue. Kinda replaced as we're using Vuex and the store. 
+	// data() => ({
+
+	// }),
+	methods: {
+
+    },
+}
+</script>
+
+<style scoped>
+h1 {
+    color: rgb(105, 255, 125);
+}
+</style>
