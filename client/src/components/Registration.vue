34dcdeaa854efe5d2e70ac385adb072560a69f83
Chris-Ack <chris_ackermann@web.de>
2021-05-31 16:29:37 +0900

    info for the registration page

--- client/src/components/Registration.vue
+++ client/src/components/Registration.vue
@@ -18,13 +18,26 @@
         />
         <br/>
         <span>Please confirm the type of account you would like to create:</span>
-        <q-select outlined v-model="model" :options="options" :dense="dense">
-        </q-select>
-            <div id="travelreg" v-if="(model === 'Traveler')">
-            <h2> Traveler Selection </h2>
+        <q-select outlined v-model="usermodel" :options="usertype" :dense="dense" />
+            <div id="travelreg" v-if="(usermodel === 'Traveler')">
+                <br/>
+                <span>You're all set! Please confirm to finish the registration.</span>
+                <br/>
+                <q-btn id="travelconfirm" color="primary" label="Finish" />
             </div>
-            <div id="guidereg" v-if="(model === 'Guide')">
-            <h2> Guide Selection </h2>
+            <div id="guidereg" v-if="(usermodel === 'Guide')">
+                <br/>          
+                <q-input filled v-model="biomodel" type="string" maxlength=160 placeholder="Please write a short bio about yourself!" hint="max 160 characters" :dense="dense" />
+                <br/>
+                <q-select filled v-model="languagemodel" multiple :options="languages" label="Languages"  hint="Multiple Options" />
+                <br/>
+                <q-select filled v-model="availabledaysmodel" multiple :options="availabledays" label="Available Days"  hint="Multiple Options" />
+                <br/>
+                <q-input filled v-model="ratemodel" type="number" placeholder="Approx. Rate" hint="in JPY, per hour" :dense="dense" />
+                <br/>
+                <span>You're all set! Please confirm to finish the registration.</span>
+                <br/>
+                <q-btn id="guideconfirm" color="primary" label="Finish" />
             </div>
     </div>
 </template>
@@ -38,10 +51,14 @@ export default {
     },
     data () {
         return {
-            model: null,
-            options: [
-            'Traveler', 'Guide'
-            ],
+            usermodel: null,
+            languagemodel: null,
+            availabledaysmodel: null,
+            ratemodel: null,
+            biomodel: null,
+            usertype: ['Traveler', 'Guide'],
+            languages: ['English', 'Japanese', 'Italian'],
+            availabledays: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
             username: ["username"],
             emailaddress: ["email address"],
             dense: true,
