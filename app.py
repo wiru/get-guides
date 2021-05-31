22a11eb2e1aae15d4c0f8597c27daa26be471f2f
Will Hauer <wiru_nihon@hotmail.com>
2021-05-31 18:28:19 +0900

    authlib added for google auth

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask_cors import CORS
 from threading import Lock
-
-from flask import Flask, jsonify, send_from_directory, render_template, session, request, \
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, url_for, redirect, jsonify, send_from_directory, render_template, session, request, \
     copy_current_request_context
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
-     
+import os
 # async_mode = None
 
 # The two lines below have to be added to the .env file to run flask
@@ -18,16 +18,30 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, \
 # Add .env to gitignore
 #
 # then do pipenv install (this installs everything mentioned in the pipfile)
-#
+# 
 # to run (any) python file, a virtual environment has to be created
 # this has to be done by running 'pipenv shell' (step has to be done after .env file is prepared)
 # After this, type "flask run" into cmd/bash
 
 
-# import socketio
-
+# AUTHLIB #
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.environ.get('OAUTH_ID'),
+    client_secret=os.environ.get('OAUTH_SECRET'),
+    access_token_url='https://accounts.google.com/o/oauth2/token',    
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid profile email'}
+)
+
+
+# SOCKET.IO #
+app.config['SECRET_KEY'] = 'secret!' # MAKE THIS HARDER FOR PRODUCTION
 socketio = SocketIO(app, cors_allowed_origins='*')
 # thread = None
 # thread_lock = Lock()
@@ -40,6 +54,34 @@ cors = CORS(app, resource={
     }
 })
 
+@app.route('/hello')
+def hello_world():
+    email = dict(session).get('email', None)
+    return 'hello'
+
+# AUTHLIB
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+    # If authorized we this route takes us to app redirect
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    resp.raise_for_status()
+    user_info = resp.json()
+    print(user_info)
+
+    # do something with the token and profile
+    session['email'] = user_info['email'] # This needs to be changed for security. We should take userinfo from above and query the database so we dont pass around googleinfo.
+    return redirect('https://localhost:8080/')
+
+
+# SOCKET.IO
 @socketio.event
 def connect():
     print('FUCKING! CONNECTED')
@@ -48,12 +90,21 @@ def connect():
 def disconnect():
     print('FUCKED OFF')
 
-# Currently stuck on long polling. Issue with server most likely relating to 
-# https://stackoverflow.com/questions/17696801/express-js-app-listen-vs-server-listen/17697134#17697134
 @socketio.event
 def Message(data):
     print(data)
 
-
 if __name__ == '__main__':
     socketio.run(app)
+
+
+# NOTES
+    # Authlib Setup
+        # pip install Authlib Flask
+        # from authlib.integrations.flask_client import OAuth
+        # oauth = OAuth(app)
+        # Make Login route
+        # Make Authorize route
+        # add oauth register
+        # register on google for oauth
+        # added 1 test email "test@test.com" on google. 
