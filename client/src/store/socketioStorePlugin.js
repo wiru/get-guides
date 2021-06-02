daba24e60e973d12a659274a4649ece6186daa52
Chris-Ack <chris_ackermann@web.de>
2021-06-02 17:40:11 +0900

    last min fix before pres

--- client/src/store/socketioStorePlugin.js
+++ client/src/store/socketioStorePlugin.js
@@ -4,6 +4,10 @@ export default function createSocketioPlugin(socket) {
     socket.on("authResult", payload => {
       console.log(payload);
       store.commit("setUserId", payload.id);
+      if (payload.path === "Search") {
+        store.commit("setUserType", "traveller")}
+      else if (payload.path === "MyProfile") {
+        store.commit("setUserType", "guide")}
       store.commit("changeView", payload.path);
       store.commit("loggedIn", payload.loggedIn);
       if (payload.path === "Registration") {
