8eed83efb1da07f30220a9a1c24c2816d8c29053
Chris-Ack <chris_ackermann@web.de>
2021-06-04 14:41:32 +0900

    post messaging fix heroku build

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -130,7 +130,7 @@ export default new Vuex.Store({
       const data = (
         await axios.get(
           // WEBLINK HERE
-          `http://localhost:5000/api/guides/search/${location}/${language}/${date}/${meme}`
+          `https://getguides.herokuapp.com/api/guides/search/${location}/${language}/${date}/${meme}`
         )
       ).data;
       state.commit("setFilteredGuides", data);
@@ -141,7 +141,7 @@ export default new Vuex.Store({
       try {
         const data = (
           // WEBLINK HERE
-          await axios.get(`http://localhost:5000/api/guides/${payload}`)
+          await axios.get(`https://getguides.herokuapp.com/api/guides/${payload}`)
         ).data;
         console.log(data);
         state.commit("setSingleGuide", data);
@@ -154,7 +154,7 @@ export default new Vuex.Store({
     async getChatLog(state, payload) {
       const data = (
         await axios.get(
-          `http://localhost:5000/api/conversations/${payload}/messages`
+          `https://getguides.herokuapp.com/api/conversations/${payload}/messages`
         )
       ).data;
       console.log("data: ", data);
@@ -168,7 +168,7 @@ export default new Vuex.Store({
       console.log("getTrav payload should be id: ", payload);
       const data = (
         await axios.get(
-          `http://localhost:5000/api/conversations/traveller/${payload}`
+          `https://getguides.herokuapp.com/api/conversations/traveller/${payload}`
         )
       ).data;
       state.commit("setChatList", data);
@@ -177,7 +177,7 @@ export default new Vuex.Store({
     async getGuideChats(state, payload) {
       const data = (
         await axios.get(
-          `http://localhost:5000/api/conversations/guide/${payload}`
+          `https://getguides.herokuapp.com/api/conversations/guide/${payload}`
         )
       ).data;
       state.commit("setChatList", data);
@@ -187,7 +187,7 @@ export default new Vuex.Store({
       const data = (
         await axios.get(
           // WEBLINK HERE
-          `http://localhost:5000/api/bookings/${this.state.userType}/${this.state.id}`
+          `https://getguides.herokuapp.com/api/bookings/${this.state.userType}/${this.state.id}`
         )
       ).data;
 
@@ -208,7 +208,7 @@ export default new Vuex.Store({
         status: "pending",
         conversation: "098123098312980"
       };
-      axios.post(`http://localhost:5000/api/bookings`, data);
+      axios.post(`https://getguides.herokuapp.com/api/bookings`, data);
     },
     // For Registration
     async travellerPackage(state, payload) {
