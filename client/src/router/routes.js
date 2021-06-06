755e8271f3087a92779cca02454676a399de2ee6
mussinbenarbia <mouhssine.benarbia@gmail.com>
2021-06-06 13:42:50 +0900

    latest build

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,20 +5,6 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/Index.vue") }]
   },
-  {
-    // WEBLINK HERE
-    path: "https://g1000.herokuapp.com/authorized",
-    component: () => import("pages/test.vue")
-  },
-  {
-    // WEBLINK HERE
-    path: "https://g1000.herokuapp.com/login",
-    name: "login",
-    beforeEnter() {
-      window.location.replace("http://localhost:5000/login");
-    }
-    //component: 'login'
-  },
 
   // Always leave this as last one,
   // but you can also remove it
