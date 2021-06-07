4192923f8fa39371f33d8a98b305a60504354c01
Chris-Ack <chris_ackermann@web.de>
2021-06-07 22:12:29 +0900

    updated links

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -213,15 +213,15 @@ export default new Vuex.Store({
     },
     // For Registration
     async travellerPackage(state, payload) {
-      axios.post(`http://localhost:5000/api/newtravellerregistration`, payload);
+      axios.post(`https://getguides.herokuapp.com/api/newtravellerregistration`, payload);
       console.log("newTravellerRegistration on front");
     },
     async guidePackage(state, payload) {
-      axios.post(`http://localhost:5000/api/newguideregistration`, payload)
+      axios.post(`https://getguides.herokuapp.com/api/newguideregistration`, payload)
       console.log("newGuideRegistration on front");
     },
     async guidePackageUpdate(state, payload) {
-      axios.post(`http://localhost:5000/api/guideupdate`, payload)
+      axios.post(`https://getguides.herokuapp.com/api/guideupdate`, payload)
       console.log("guide Update on front")
     }
   }
